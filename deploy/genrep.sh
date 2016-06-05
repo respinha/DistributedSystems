ee5bae2026990063dba21d385777c1fa961cc824
David Silva <davidmsilva@ua.pt>
2016-06-05 20:09:35 +0100

    Build and deploy script.

---
+++ deploy/genrep.sh
@@ -0,0 +1,4 @@
+java -Djava.rmi.server.codebase=file:pt/ua/sd/ropegame/\
+	 -Djava.rmi.server.useCodebaseOnly=false\
+     -Djava.security.policy=java.policy\
+     pt.ua.sd.ropegame.genrepository.GeneralRepositoryServer configs.xml
